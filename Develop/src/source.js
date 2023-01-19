df16af6df4d578615b278850505d6e6acfd08091
CaptureB <airdavid62@gmail.com>
2023-01-19 13:00:04 -0700

    fixed typo erros that didnt open emails and github

--- Develop/src/source.js
+++ Develop/src/source.js
@@ -7,7 +7,7 @@ const generateHTML = (team) => {
         <h5 class="card-title">${intern.getName()}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${intern.getRole()}</h6>
         <p class="card-text">ID:${intern.getId()}</p>
-        <a href="${intern.getEmail()}" class="card-link">Email:${intern.getEmail()}</a>
+        <a class = "list-group-class">Eamil: <a href ="mailto:${intern.getEmail()}">${intern.getEmail()}</a>
         <a href="" class="card-link">School:${intern.getSchool()}</a>
       </div>`
     }
@@ -20,8 +20,8 @@ const generateHTML = (team) => {
         <h5 class="card-title">${engineer.getName()}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${engineer.getRole()}</h6>
         <p class="card-text">ID: ${engineer.getId()}</p>
-        <a href="${engineer.getEmail()}" class="card-link">Email: ${engineer.getEmail()}</a>
-        <a href="https://github/${engineer.getGitHub()}" class="card-link">GitHub: ${engineer.getGitHub()}</a>
+        <a class ="list-group-class">Email: <a href = "mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
+        <a href="https://github.com/${engineer.getGitHub()}" class="card-link">GitHub: ${engineer.getGitHub()}</a>
       </div>
         `
     }
@@ -33,7 +33,7 @@ const generateHTML = (team) => {
         <h5 class="card-title">${manager.getName()}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${manager.getRole()}</h6>
         <p class="card-text">ID: ${manager.getId()}</p>
-        <a href="${manager.getEmail()}" class="card-link">Email: ${manager.getEmail()}</a>
+        <a href="mailto:${manager.getEmail()}" class="card-link">${manager.getEmail()}</a>
         <a href="#" class="card-link">Room :${manager.getRoomNumber()}</a>
       </div>`
     }
@@ -67,7 +67,7 @@ const generateHTML = (team) => {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-  <title>Bootstrap demo</title>
+  <title>Team Gen</title>
 </head>
 <body>
 <header class="p-5 mb-4 header bg-light">
