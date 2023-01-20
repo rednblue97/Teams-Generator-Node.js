75aa030b6037652397547daa7ac52b1233c6ce5e
David Garcia <114364879+CaptureB@users.noreply.github.com>
2023-01-20 09:14:06 -0700

    final comments

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,18 @@
+//Requires the inquirer package downloaded by npm i
 const inquirer = require('inquirer');
 const fs = require("fs");
+//Generates html template from the source.js file
 const generateHTML = require('./src/source.js')
 
 const teamData =[];
-
+//Calling upon the manager, intern, and engineer libraries
 const Manager = require('./lib/manager.js');
 const Intern = require('./lib/intern.js');
 const Engineer = require('./lib/engineer.js');
-
+//Uses async for promises to mnake sure the prompts are answered
 const userPrompts = async () => {
 const userAnswers = await inquirer
+//Prompts for name,id, email and role
     .prompt([
         {
             type:'input',
@@ -33,7 +36,8 @@ const userAnswers = await inquirer
             choices: ['Engineer','Intern','Manager'],
         },
     ])
-
+//If statements if the user selects intern, manager, and engineer
+    //If the user selects intern, it will prompt what school they go to
 
     if(userAnswers.role === "Intern") {
         const internAw = await inquirer
@@ -50,8 +54,9 @@ const userAnswers = await inquirer
             userAnswers.email,
             internAw.school
         );
+        //pushes the collected data
         teamData.push(userIntern);
-
+ //If the user selects the engineer it will prompt for the github username to be deployed in the html
     } else if (userAnswers.role === 'Engineer') {
         const engineerAw = await inquirer
         .prompt([
@@ -67,8 +72,9 @@ const userAnswers = await inquirer
             userAnswers.email,
             engineerAw.gitHub,
         );
+        //pushes the collected data
         teamData.push(userEngineer);
-    
+    //If the user selects the manager it will prompt for the room number
     } else if (userAnswers.role === 'Manager') {
         const managerAw = await inquirer
         .prompt([
@@ -84,14 +90,15 @@ const userAnswers = await inquirer
             userAnswers.email,
             managerAw.roomNumber
         )
+        //pushes the collected data
         teamData.push(userManager);
        
     }
 };
-
+//Waits for the prompts to be completed
 async function gen() {
     await userPrompts()
-
+//Gives the user after completing one team member, have the option to generate another team member or generate the html
 const finalAw = await inquirer
     .prompt([
         {
@@ -102,6 +109,7 @@ const finalAw = await inquirer
         }
     ]);
 
+//If the user selects to add another team member, it will return to generate another team member, if not, it will create the team
     if (finalAw.addTeamMember === 'Add another team member') {
         return gen()
     }
@@ -111,7 +119,7 @@ const finalAw = await inquirer
 }
 
 gen();
-
+// Generates the html, will be labeled as index.html, collected all the team data
 function teamCreate () {
     fs.writeFileSync(
         './index.html',
